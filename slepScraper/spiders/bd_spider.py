98ceb3c59270e7d00b4702140292beac88b8abd1
elebumm <lmenelaws112@gmail.com>
2017-07-19 21:33:45 -0400

    Updated spider to follow PEP8 standards

--- slepScraper/spiders/bd_spider.py
+++ slepScraper/spiders/bd_spider.py
@@ -18,9 +18,7 @@ class BDSpider(scrapy.Spider):
             relative_url = business.css('a::attr(href)').extract_first()
             absolute_url = response.urljoin(relative_url)
 
-
             yield Request(absolute_url, callback=self.parse_page, meta={'URL': absolute_url})
-
 
         next_page = response.css('a[title="next"]::attr(href)').extract_first()
         if next_page is not None:
@@ -29,7 +27,6 @@ class BDSpider(scrapy.Spider):
             yield scrapy.Request(next_page, callback=self.parse)
         else:
             print('no more things to scrape')
-
 
     def parse_page(self, response):
         url = response.meta.get('URL')
@@ -42,23 +39,22 @@ class BDSpider(scrapy.Spider):
         # List of all social links that the business has
         social = response.css('.entry-content > div:nth-child(2) a::attr(href)').extract()
 
-
         yield {
-            'title' : response.css('h1.entry-title::text').extract_first().strip(),
-            'description' : response.css('p.mb-double::text').extract_first(),
-            'phone_number' : response.css('div.mb-double ul li::text').extract_first(default="").strip(),
-            'email' : response.css('div.mb-double ul li a::text').extract_first(default=""),
-            'address' : locations[0].strip(),
-            'city' : city_province.split(' ', 1)[0].replace(',',''),
-            'province' : city_province.split(' ', 1)[1].replace(',','').strip(),
-            'zip_code' : locations[2].strip(),
-            'website' : response.css('.entry-content > div:nth-child(2) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)::attr(href)').extract_first(default=''),
-            'facebook' : response.css('.entry-content > div:nth-child(2) > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1)::attr(href)').extract_first(default=''),
-            'twitter' : response.css('.entry-content > div:nth-child(2) > ul:nth-child(2) > li:nth-child(3) > a:nth-child(1)::attr(href)').extract_first(default=''),
-            'linkedin' : response.css('.entry-content > div:nth-child(2) > ul:nth-child(2) > li:nth-child(4) > a:nth-child(1)::attr(href)').extract_first(default=''),
-            'year' : response.css('.list-border > li:nth-child(1)::text').extract_first(default="").strip(),
-            'employees' : response.css('.list-border > li:nth-child(2)::text').extract_first(default="").strip(),
-            'key_contact' : response.css('.list-border > li:nth-child(3)::text').extract_first(default="").strip(),
-            'naics' : response.css('.list-border > li:nth-child(4)::text').extract_first(default="").strip(),
-            'tags' : response.css('ul.biz-tags li a::text').extract(),
+            'title': response.css('h1.entry-title::text').extract_first().strip(),
+            'description': response.css('p.mb-double::text').extract_first(),
+            'phone_number': response.css('div.mb-double ul li::text').extract_first(default="").strip(),
+            'email': response.css('div.mb-double ul li a::text').extract_first(default=""),
+            'address': locations[0].strip(),
+            'city': city_province.split(' ', 1)[0].replace(',', ''),
+            'province': city_province.split(' ', 1)[1].replace(',', '').strip(),
+            'zip_code': locations[2].strip(),
+            'website': response.css('.entry-content > div:nth-child(2) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)::attr(href)').extract_first(default=''),
+            'facebook': response.css('.entry-content > div:nth-child(2) > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1)::attr(href)').extract_first(default=''),
+            'twitter': response.css('.entry-content > div:nth-child(2) > ul:nth-child(2) > li:nth-child(3) > a:nth-child(1)::attr(href)').extract_first(default=''),
+            'linkedin': response.css('.entry-content > div:nth-child(2) > ul:nth-child(2) > li:nth-child(4) > a:nth-child(1)::attr(href)').extract_first(default=''),
+            'year': response.css('.list-border > li:nth-child(1)::text').extract_first(default="").strip(),
+            'employees': response.css('.list-border > li:nth-child(2)::text').extract_first(default="").strip(),
+            'key_contact': response.css('.list-border > li:nth-child(3)::text').extract_first(default="").strip(),
+            'naics': response.css('.list-border > li:nth-child(4)::text').extract_first(default="").strip(),
+            'tags': response.css('ul.biz-tags li a::text').extract(),
         }
